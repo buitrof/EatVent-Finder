d2a0219cd8f07418637969ef5455307082c3a61b
fbuitron <franciscobuitron21@gmail.com>
2020-02-04 10:43:38 -0800

    updated files to contain original files

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+const base = 'https://app.ticketmaster.com/discovery/v1/events.json?'
+const api = '&apikey=TqPIy9GVqFGripe0WWubsOyxCKeuGtZf'
+let keywords = ''
+let id = 'input_text'
+
+function getKeywords(id) {
+  keywords = document.getElementById('input_text').value
+  keywords = keywords.replace(/\s+/g, '+')
+  return keywords
+}
+
+function buildURL() {
+  getKeywords('input_text')
+  let url = base + getKeywords() + api
+  console.log(getKeywords())
+  console.log(url)
+}
+
+document.getElementById('submit').addEventListener('click', event => {
+  event.preventDefault()
+  buildURL()
+  document.getElementById('input_text').value = ''
+})
