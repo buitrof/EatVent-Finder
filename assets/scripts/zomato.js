958a22163b8307794a3ff3f3e67b4a13906bed84
Jie Yan <jieyan620@gmail.com>
2020-02-06 12:17:36 -0800

    edit restaurants picture

--- assets/scripts/zomato.js
+++ assets/scripts/zomato.js
@@ -2,32 +2,24 @@
 const L_B_ZOMATO = 'https://developers.zomato.com/api/v2.1/search?'
 const K_ZOMATO = 'apikey=39e17219549ea152e0fb9205ede5e31f'
 const S_RATING = 'sort=rating'
-
-
 let listOfRest = []
 let latt = ''
 let long = ''
-
 class Restaurant {
-
-    constructor(restaurant) {
-        this.name = restaurant.name
-        this.address = restaurant.location.address
-        this.phone_numbers = restaurant.phone_numbers
-        this.latitude = null
-        this.longitude = null
-        this.cuisines = restaurant.cuisines
-        this.highlights = restaurant.highlights
-        this.user_rating = restaurant.user_rating
-        this.photos = restaurant.photos
-        //console.log(this)
-    }
-
-
-
+  constructor(restaurant) {
+    this.name = restaurant.name
+    this.user_rating = restaurant.user_rating
+    this.address = restaurant.location.address
+    this.phone_numbers = restaurant.phone_numbers
+    this.latitude = null
+    this.longitude = null
+    this.cuisines = restaurant.cuisines
+    this.highlights = restaurant.highlights
+    // this.user_rating = restaurant.user_rating
+    this.photos = restaurant.photos
+    //console.log(this)
+  }
 }
-
-
 function getRestaurantChoices(latt, long) {
   let link = `${L_B_ZOMATO}lat=${latt}&lon=${long}&${S_RATING}&${K_ZOMATO}`
   fetch(link)
@@ -43,16 +35,36 @@ function getRestaurantChoices(latt, long) {
           listOfRest.push(rest)
           BuildEventCard(rest)
         })
-        .catch(e => console.error(e))
+      } else {
+        console.log('Nothing found')
+      }
+    })
+    .catch(e => console.error(e))
 }
-
 function BuildEventCard(rest) {
   console.log(rest)
   let restaurantResultElem = document.createElement('div')
   restaurantResultElem.className = 'uk-card uk-card-hover uk-card-body uk-grid'
   restaurantResultElem.innerHTML = `
             <div>
-                <img src="${rest.photos[0].photo.url}" alt="Image" srcset="" class="card-image">
+              <div class="uk-position-relative uk-visible-toggle uk-light food-image " tabindex="-1" uk-slideshow>
+               <ul class="uk-slideshow-items food-image">
+               <li>
+                 <img data-src="${rest.photos[0].photo.url}" class="uk-card-media-left food-image" alt="" uk-cover uk-img="target: !ul > :last-child, !* +*">
+              </li>
+              <li>
+                 <img data-src="${rest.photos[1].photo.url}" class="uk-card-media-left card-image" alt="" uk-cover uk-img="target: !* -*, !* +*">
+               </li>
+               <li>
+                 <img data-src="${rest.photos[2].photo.url}" class="uk-card-media-left card-image" alt="" uk-cover uk-img="target: !* -*, !ul > :first-child">
+              </li>
+              <li>
+                 <img data-src="${rest.photos[3].photo.url}" class="uk-card-media-left card-image" alt="" uk-cover uk-img="target: !* -*, !ul > :first-child">
+              </li>
+              </ul>
+              <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+              <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+              </div>
                 <h3 class="uk-card-title">${rest.name}</h3>
                 <p>Rating: ${rest.user_rating.aggregate_rating}</p>
                 <p>Highlights: ${rest.highlights}</p>
@@ -63,35 +75,4 @@ function BuildEventCard(rest) {
                  `
   document.getElementById('search-results').append(restaurantResultElem)
 }
-      let restaurantResultElem = document.createElement('div')
-      restaurantResultElem.className = 'uk-card uk-card-hover uk-card-body uk-grid'
-      function imgRestaurant(imgs) {
-        var expandImg = document.getElementById("expandedImg");
-        var imgText = document.getElementById("imgtext");
-        expandImg.src = imgs.src;
-        imgText.innerHTML = imgs.alt;
-        expandImg.parentElement.style.display = "block";
-      }
-      restaurantResultElem.innerHTML = `
-                <div>
-                <div class="row">
-                  <div class="column">
-                     <img src="${restaurant.restaurant.photos[0].photo.url}" alt=""  class="card-image" style="width:100%" onclick="imgRestaurant(this);">
-                  </div>
-                  <div class="column">
-                      <img src="${restaurant.restaurant.photos[1].photo.url}" alt=""  class="card-image" style="width:100%" onclick="imgRestaurant(this);">
-                 </div>
-                 <div class="column">
-                     <img src="${restaurant.restaurant.photos[2].photo.url}" alt=""  class="card-image" style="width:100%" onclick="imgRestaurant(this);">
-                 </div>
-                 <div class="column">
-                     <img src="${restaurant.restaurant.photos[3].photo.url}" alt=""  class="card-image" style="width:100%" onclick="imgRestaurant(this);">
-                 </div>
-               </div>
-                    </div>
-                    `
-
-      document.getElementById('search-results').append(restaurantResultElem)
-
-
 getRestaurantChoices('47.60577', '-122.329437')
